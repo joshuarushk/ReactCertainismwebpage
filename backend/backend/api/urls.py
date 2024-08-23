1df971993f5e2a46def124876308823d0d953228
joshuarushk <144501381+joshuarushk@users.noreply.github.com>
2024-08-23 13:37:54 -0400

    Delete backend directory

--- backend/backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CorePrinciplesView, PropositionsView
-
-urlpatterns = [
-    path('core-principles/', CorePrinciplesView.as_view(), name='core-principles'),
-    path('propositions/', PropositionsView.as_view(), name='propositions'),
-]
