ba104227072d2840cf881aa83863538d47165388
joshuarushk <144501381+joshuarushk@users.noreply.github.com>
2024-08-23 13:30:13 -0400

    Main app

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CorePrinciples from './components/CorePrinciples';
+import Propositions from './components/Propositions';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Certainism Philosophical Framework</h1>
+            <CorePrinciples />
+            <Propositions />
+        </div>
+    );
+}
+
+export default App;
