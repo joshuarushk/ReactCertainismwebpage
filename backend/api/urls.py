6f43f745ef7851366fbd93530453812d05b05fdb
joshuarushk <144501381+joshuarushk@users.noreply.github.com>
2024-08-23 14:00:39 -0400

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CorePrinciplesView, PropositionsView
+from .views import CorePrinciplesView, FundamentalTenetsView, PropositionsView, TheoreticalFoundationsView, PracticalApplicationsView
 
 urlpatterns = [
     path('core-principles/', CorePrinciplesView.as_view(), name='core-principles'),
+    path('fundamental-tenets/', FundamentalTenetsView.as_view(), name='fundamental-tenets'),
     path('propositions/', PropositionsView.as_view(), name='propositions'),
+    path('theoretical-foundations/', TheoreticalFoundationsView.as_view(), name='theoretical-foundations'),
+    path('practical-applications/', PracticalApplicationsView.as_view(), name='practical-applications'),
 ]
